994333695a051f529384956d10490e0d9df145c6
ryan <rkram5424@gmail.com>
2017-02-02 22:55:39 -0600

    Started adding subcomponents.

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import SplitterLayout from 'react-splitter-layout';
 
+import Instrument from '../../Instrument/components/Instrument';
+import Mixer from '../../Mixer/components/Mixer';
+import Pianoroll from '../../Pianoroll/components/Pianoroll';
+import Pattern from '../../Pattern/components/Pattern';
+
 export const HomeView = () => (
   <SplitterLayout>
-  <SplitterLayout vertical='true'>
-    <div>Pattern</div>
-    <div>Instrument</div>
-  </SplitterLayout>
-  <SplitterLayout vertical='true'>
-    <div>Pianoroll</div>
-    <div>Mixer</div>
-  </SplitterLayout>
+    <SplitterLayout vertical='true'>
+      <Pattern />
+      <Instrument />
+    </SplitterLayout>
+    <SplitterLayout vertical='true'>
+      <Pianoroll />
+      <Mixer />
+    </SplitterLayout>
   </SplitterLayout>
 )
 
