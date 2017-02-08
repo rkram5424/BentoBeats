cdecdaba7d4fefd45a36641d71b70572d1ee33fe
rkram5424 <rkram5424@gmail.com>
2017-02-07 20:32:52 -0600

    Added react-bootstrap.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,36 @@
 import React from 'react'
 import { IndexLink, Link } from 'react-router'
+import { Button, ButtonToolbar, ButtonGroup } from 'react-bootstrap'
 import './Header.scss'
 
 export const Header = () => (
+  // <div>
+  //   <Link to='/pattern' activeClassName='route--active'>
+  //     Pattern
+  //   </Link>
+  //   {' · '}
+  //   <Link to='/pianoroll' activeClassName='route--active'>
+  //     Pianoroll
+  //   </Link>
+  //   {' · '}
+  //   <Link to='/instrument' activeClassName='route--active'>
+  //     Instrument
+  //   </Link>
+  //   {' · '}
+  //   <Link to='/mixer' activeClassName='route--active'>
+  //     Mixer
+  //   </Link>
+  // </div>
+  
   <div>
-    <Link to='/pattern' activeClassName='route--active'>
-      Pattern
-    </Link>
-    {' · '}
-    <Link to='/pianoroll' activeClassName='route--active'>
-      Pianoroll
-    </Link>
-    {' · '}
-    <Link to='/instrument' activeClassName='route--active'>
-      Instrument
-    </Link>
-    {' · '}
-    <Link to='/mixer' activeClassName='route--active'>
-      Mixer
-    </Link>
+    <ButtonToolbar>
+      <ButtonGroup>
+        <Button>Pattern</Button>
+        <Button>Pianoroll</Button>
+        <Button>Instrument</Button>
+        <Button>Mixer</Button>
+      </ButtonGroup>
+    </ButtonToolbar>
   </div>
 )
 
